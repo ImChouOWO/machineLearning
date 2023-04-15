6fbf8194abd6c2e69d4717d92c93f900eb80a209
Chou_isme <as0930859852@gmail.com>
2023-04-15 23:53:11 +0800

    logisticRegression

--- code/logisticRegression.py
+++ code/logisticRegression.py
@@ -63,9 +63,11 @@ def logisticRegression(X_train, X_test, y_train, y_test):
     ppn = Perceptron(eta0=0.1, random_state=1)
     ppn.fit(X_train_std, y_train)
     y_pred = ppn.predict(X_test_std)
-    print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
     #计算模型的准确性
+    print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
     print('Accuracy: %.3f' % ppn.score(X_test_std, y_test))
+
+    # 畫圖
     X_combined_std = np.vstack((X_train_std, X_test_std))
     y_combined = np.hstack((y_train, y_test))
     plot_decision_regions(X_combined_std, y_combined,
@@ -74,7 +76,7 @@ def logisticRegression(X_train, X_test, y_train, y_test):
     plt.ylabel('petal width [standardized]')
     plt.legend(loc='upper left')
     plt.tight_layout()
-    # plt.savefig('images/03_06.png', dpi=300)
+    plt.savefig('images/03_06.png', dpi=300)
     plt.show()
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
